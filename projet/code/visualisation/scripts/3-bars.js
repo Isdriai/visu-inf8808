3f79c9913e6c064aef68be13c46a16fe65a55174
Picot <picot.rodolphe@gmail.com>
2020-04-14 00:43:58 -0400

    pb refacto bars

--- projet/code/visualisation/scripts/3-bars.js
+++ projet/code/visualisation/scripts/3-bars.js
@@ -1,18 +1,5 @@
 "use strict";
 
-
-
-
-
-function createAxes() {
-
-
-
-    //return [xAxis, yAxis]
-}
-
-
-
 function createBarChart(barsGroup, currentData, [xAxis, yAxis]) {
 
     barsGroup.selectAll("rect")
@@ -29,41 +16,44 @@ function createBarChart(barsGroup, currentData, [xAxis, yAxis]) {
      .on("mouseout", tip.hide)
   }
 
-function countBySect(dict) {
-    return Object.keys(dict).map(sect => ({
-        name: sect,
-        count: d3.sum(Object.keys(dict[sect]).map(priv => 
-            d3.sum(Object.keys(dict[sect][priv]).map(typePub => 
-                 d3.sum(Object.keys(dict[sect][priv][typePub]).map(actor => dict[sect][priv][typePub][actor]))
-            ))
+function countByPriv(dictSect) {
+    return Object.keys(dictSect).map(priv => ({
+        name: priv,
+        count: d3.sum(Object.keys(dictSect[priv]).map(typePub => 
+             d3.sum(Object.keys(dictSect[priv][typePub]).map(actor => dictSect[priv][typePub][actor]))
         ))
     }))
 }
 
-function initBars(barsGroup, dict, privates) {
-    
-    var dataBySect = countBySect(dict)
+function countBySect(dict) {
+    return Object.keys(dict).map(sect => ({
+        name: sect,
+        count: d3.sum(countByPriv(dict[sect]).map(priv => priv.count))
+    }))
+}
 
-    var x = d3.scaleBand().range([0, 1000]).round(0.05);
-  var y = d3.scaleLinear().range([800, 0]);
-
-    x.domain(dataBySect.map(s => s.name))
-    y.domain(dataBySect.map(s => s.count))
+function createAxesLadders(data, height, width, funX) {
+    var x = d3.scaleBand().range([0, width]).round(0.05)
+    var y = d3.scaleLinear().range([height, 0])
+    x.domain(data.map(funX))
+    y.domain(data.map(d => d.count))
 
     var xAxis = d3.axisBottom(x);
-
     var yAxis = d3.axisLeft(y)
 
+    return [x, y, xAxis, yAxis]
+}
 
+function drawBars(barsGroup, data, x, y, xAxis, yAxis, height, width) {
     barsGroup.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + 800 + ")")
+        .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
       .selectAll("text")
         .style("text-anchor", "end")
         .attr("dx", "-.8em")
         .attr("dy", "-.55em")
-        .attr("transform", "rotate(-90)" );
+        .attr("transform", "rotate(-90)" )
   
     barsGroup.append("g")
         .attr("class", "y axis")
@@ -73,16 +63,37 @@ function initBars(barsGroup, dict, privates) {
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Value ($)");
+        .text("Value ($)")
   
     barsGroup.selectAll("bar")
-        .data(dataBySect)
-      .enter().append("rect")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "rect")
         .style("fill", "steelblue")
-        .attr("x", function(d) { return x(d.name); })
+        .attr("x", d => x(d.name))
         .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.count); })
-        .attr("height", function(d) { return 800 - y(d.count); });
-  
-  
+        .attr("y", d => y(d.count))
+        .attr("height", d => height - y(d.count))
+}
+
+function setOnclick(barsGroup, dict, height, width) {
+    barsGroup.selectAll(".rect")
+        .on("click", d => {
+            var [x, y, xAxis, yAxis] = createAxesLadders(dict[d.name], height, width, d => d.name)
+            drawBars(barsGroup, x, y, xAxis, yAxis, height, width)
+        })
+}
+
+function initBars(barsGroup, dict, privates) {
+    
+    var dataBySect = countBySect(dict)
+
+    var svgBars = d3.select("#svgBars")
+    var height = svgBars.attr("height")
+    var width = svgBars.attr("width")
+
+    var [x, y, xAxis, yAxis] = createAxesLadders(dataBySect, height, width, d => d.name)
+
+    drawBars(barsGroup, x, y, xAxis, yAxis, height, width)
+    setOnclick(barsGroup, dict, height, width)
 }
